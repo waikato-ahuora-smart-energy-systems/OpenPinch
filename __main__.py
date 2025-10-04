cde55d8e8384d53e1d41cff13966b49d32a0d545
Tim Walmsley <tim.walmsley@waikato.ac.nz>
2025-10-04 14:12:37 +0100

    Copied workbooks.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,18 @@
-from pathlib import Path
+# from pathlib import Path
 
+# from OpenPinch import PinchProblem
+
+# if __name__ == "__main__":
+#     P = PinchProblem(
+#         problem_filepath = Path(__file__).resolve().parents[0] / "Excel_Version" / "Data_input_template.xlsx",
+#         results_dir = Path(__file__).resolve().parents[0] / "results",
+#     )
+
+from pathlib import Path
 from OpenPinch import PinchProblem
 
-if __name__ == "__main__":
-    P = PinchProblem(
-        problem_filepath = Path(__file__).resolve().parents[0] / "Excel_Version" / "Data_input_template.xlsx",
-        results_dir = Path(__file__).resolve().parents[0] / "results",
-    )
+problem = PinchProblem(
+    problem_filepath=Path("examples/OpenPinchWkbs/pulp_mill.xlsb"),
+    results_dir=Path("results"),
+    run=True,
+)
