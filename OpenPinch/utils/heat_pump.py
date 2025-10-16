f8b0acb6ceabd71169263482fa03c18b9ef2ba9d
Tim Walmsley <tim.walmsley@waikato.ac.nz>
2025-10-16 13:17:12 +0200

    Added docstrings for heat pump

--- OpenPinch/utils/heat_pump.py
+++ OpenPinch/utils/heat_pump.py
@@ -1,3 +1,5 @@
+"""Convenience utilities for evaluating a simple heat pump cycle."""
+
 from CoolProp.Plots import SimpleCompressionCycle
 
 
@@ -9,6 +11,18 @@ def get_hp_cycle_COP(
     dT_sc=5, 
     eta_com=0.7,
 ):
+    """Compute the heating coefficient of performance (COP).
+
+    Args:
+        fld: Working fluid name understood by CoolProp.
+        Te: Evaporator saturation temperature in Celsius.
+        Tc: Condenser saturation temperature in Celsius.
+        dT_sh: Superheat at evaporator outlet in Kelvin.
+        dT_sc: Subcooling at condenser outlet in Kelvin.
+        eta_com: Compressor isentropic efficiency (0–1).
+    Return:
+        COP_heating: Coefficient of Performance for heating.
+    """
     Te += 273.15
     Tc += 273.15
     cycle = SimpleCompressionCycle(f'HEOS::{fld}', 'PH', unit_system='EUR')
@@ -21,5 +35,9 @@ def get_hp_cycle_COP(
         fluid=f'HEOS::{fld}', 
         SI=True
     )
-    print(f"COP heating:{cycle.COP_heating()}")
-get_hp_cycle_COP()
+    COP_heating = cycle.COP_heating()
+    return COP_heating
+
+print(
+    get_hp_cycle_COP()
+)
