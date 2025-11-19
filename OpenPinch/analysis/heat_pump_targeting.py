Fixed the calculation of ambient air as a source.

OpenPinch/analysis/heat_pump_targeting.py
@@ -92,7 +92,7 @@ def get_heat_pump_targets(
         dT_phase_change=zone_config.DT_PHASE_CHANGE,
         Q_ls=np.array([res.Q_amb]),
         dt_cont=zone_config.DT_ENV_CONT,
-        is_hot=True if res.Q_amb > 0 else False,
+        is_hot=True if res.Q_amb >= -tol else False,
         is_process_stream=True,
         prefix="AIR",
     )
@@ -552,7 +552,7 @@ def _get_optimal_min_evap_T(
         "T_evap": T_evap, 
         "Q_evap": Q_evap, 
         "cop": cop,
-        "Q_amb": Q_evap.sum() - (np.abs(args.H_hot).max() - args.Q_amb_max),
+        "Q_amb": max(Q_evap.sum() - (np.abs(args.H_hot).max() - args.Q_amb_max), args.Q_amb_max),
     }
 
 

OpenPinch/analysis/problem_table_analysis.py
@@ -72,11 +72,7 @@ def get_utility_heat_cascade(
     is_shifted: bool = True,
 ) -> Dict[str, np.ndarray]:
     """Prepare and calculate the utility heat cascade a given set of hot and cold utilities."""
-    pt_ut = ProblemTable(
-        {
-            PT.T.value: T_int_vals
-        }
-    )
+    pt_ut = ProblemTable({PT.T.value: T_int_vals})
     _problem_table_algorithm(pt_ut, hot_utilities, cold_utilities, is_shifted)
 
     h_net_values = pt_ut.col[PT.H_NET.value]